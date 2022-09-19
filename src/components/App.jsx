a9fc241dab713e2b8d8c56e17e891add812b861e
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-09-19 17:24:41 +0300

    Phonebook jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  handleInputChange = event => {
+    this.setState({ name: event.currentTarget.value });
+    console.log(event.currentTarget.value);
+  };
+  render() {
+    return (
+      <>
+        <p className="title">Phonebook</p>
+        <form className="phonebookForm">
+          <label htmlFor="name" className="labelInputName">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            className="inputName"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+
+          <button type="submit">Add contact</button>
+        </form>
+        <ul className="title">Contacts</ul>
+      </>
+    );
+  }
+}
+export default App;
